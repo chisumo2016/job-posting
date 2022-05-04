882172a5fc89b2253be20118d93a9b04c9e324e9
bernard chisumo <bchisumo74@yahoo.com>
2022-05-04 15:48:04 +0100

    Listing Modelling

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         User::factory(5)->create();
+         Listing::factory(10)->create();
+
     }
 }
